a7ef292c16d6b5dd947c71eb271ee937d7d5841f
yottaes <ctf0000000@gmail.com>
2026-02-22 03:39:11 +0700

    update: neo-tree root behavior

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Don't auto-change cwd to git root — use the directory you opened nvim from
+vim.g.root_spec = { "cwd" }
+
 vim.opt.scrolloff = 15
 vim.opt.sidescrolloff = 5
 
